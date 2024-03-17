bb2700d0f2975295394e1ec857651ed975f71545
Chizy <chizy7@gmail.com>
2024-03-17 19:31:37 -0400

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Chizaram Chibueze on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
