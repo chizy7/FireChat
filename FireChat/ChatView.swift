9986b508349bbecae3f8a9127c63331ceacc1be7
Chizy <chizy7@gmail.com>
2024-03-21 00:38:06 -0400

    Created UI for sending a message

--- FireChat/ChatView.swift
+++ FireChat/ChatView.swift
@@ -37,6 +37,11 @@ struct ChatView: View {
                     }
                 }
             }
+            .safeAreaInset(edge: .bottom) {
+                SendMessageView { messageText in
+                    // TODO: Save message to Firestore
+                }
+            }
         }
     }
 }
@@ -69,6 +74,38 @@ struct MessageRow: View {
             )
             .padding(isOutgoing ? .trailing : .leading, 12) // Set padding based on outgoing status
             .containerRelativeFrame(.horizontal, count: 7, span: 5, spacing: 0, alignment: isOutgoing ? .trailing : .leading) // Set message size relative to container (the scroll view width in this case)
+    }
+}
+
+struct SendMessageView: View {
+    var onSend: (String) -> Void // Closure called with a message passed in when send message button is tapped
+    
+    @State private var messageText: String = "" // Local state managed var to hold the message text as user types
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            TextField("Message", text: $messageText, axis: .vertical) // Message text field
+                .padding(.leading)
+                .padding(.trailing, 4)
+                .padding(.vertical, 8)
+            
+            // Send message button
+            Button {
+                onSend(messageText) // Call onSend closure passing in the message text when send button is tapped
+                messageText = "" // Clear the message text after being sent
+            } label: {
+                Image(systemName: "arrow.up.circle.fill") // Use arrow image from SFSymbols
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .bold()
+                    .padding(4)
+            }
+            .disabled(messageText.isEmpty) // Disable button if text is empty
+        }
+        .overlay(RoundedRectangle(cornerRadius: 19).stroke(Color(uiColor: .systemGray2)))
+        .padding(.horizontal)
+        .padding(.vertical, 8)
+        .background(.thinMaterial) // Add material to background
     }
 }
 
